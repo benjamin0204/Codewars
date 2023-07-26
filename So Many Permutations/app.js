32fba3e5a986c96795765d9f030a633e90ec661f
benjamin0204 <benjamin020497@gmail.com>
2023-07-26 10:22:39 +0100

    solution

---
+++ So Many Permutations/app.js
@@ -0,0 +1,28 @@
+function permutations(string) {
+  if (!string || typeof string !== "string") {
+    return "Please enter a string";
+  }
+  if (string.length < 2) {
+    return new Array(string);
+  }
+
+  let answers = [];
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string[i];
+
+    if (string.indexOf(char) != i) continue;
+
+    let remainingChars =
+      string.slice(0, i) + string.slice(i + 1, string.length);
+
+    for (let permutation of permutations(remainingChars)) {
+      answers.push(char + permutation);
+    }
+  }
+  return answers;
+}
+
+console.log(permutations("a"));
+console.log(permutations("ab"));
+console.log(permutations("aabb"));
